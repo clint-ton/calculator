f824b3af721b67a7f824c4b1fe7f83d75812e4f6
clinton <cjw237@uclive.ac.nz>
2021-12-20 13:56:21 +1300

    operator funcs and basic ui

---
+++ main.js
@@ -0,0 +1,59 @@
+const buttonLabels = [
+    ['AC', '+/-', '←', '^'],
+    ['7', '8', '9', '÷'],
+    ['4', '5', '6', 'x'],
+    ['1', '2', '3', '-'],
+    ['0', '.', '=', '+'],
+]
+
+function add (x, y) {
+    return x + y;
+}
+
+function subtract (x, y) {
+    return x - y;
+}
+
+function multiply (x, y) {
+    return x * y;
+}
+
+function divide (x, y) {
+    return x / y;
+}
+
+function operate(operator, x, y) {
+    switch (operator) {
+        case '+':
+            return add(x, y);
+            break;
+        case '-':
+            return subtract(x, y);
+            break;
+        case '*':
+            return multiply(x, y);
+            break;
+        case '/':
+            return divide(x, y);
+            break;
+        default:
+            return 'INVALID OPERATOR'
+    };
+}
+
+function drawKeypad () {
+    const buttons = document.querySelector(".buttons")
+    for (let i = 0; i < 5; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < 4; j++) {
+            const button = document.createElement('button');
+            button.textContent = buttonLabels[i][j];
+            row.appendChild(button);
+        }
+        buttons.appendChild(row);
+    }
+}
+
+drawKeypad()
+console.log(buttonLabels)
